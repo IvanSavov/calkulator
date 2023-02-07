9cd2f8e4ba8be78b01bb36f91acfdd3ac6d9f8ef
Ivan Savov <savovivan60@gmail.com>
2023-02-07 15:24:46 +0200

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = document.getElementById("k").value;
+let b = document.getElementById("kk").value;
+let c = document.getElementById("kkk");
+a = Number(a);
+b = Number(b);
+console.log(a);
+console.log(b);
+console.log(a + b);
+c.innerText = a + b;
