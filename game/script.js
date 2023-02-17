84fcd7c32c9c8de82061166aec3471e46af3507b
Ivan Savov <savovivan60@gmail.com>
2023-02-17 11:29:55 +0200

    d

--- game/script.js
+++ game/script.js
@@ -5,14 +5,31 @@ let ny = 3;
 let sqside = 50
 let herox = 2;
 let heroy = 1;
+let heroimg = new Image();
+heroimg.src = "hero.png";
 canvas.height = nx * sqside;
 canvas.weight = ny * sqside;
+
+
+function drawMap() {
+context.clearRect(0, 0, canvas.width, canvas.height);
+context.drawImage(heroimg,herox* sqside,heroy * sqside,sqside,sqside);
 for (let i = 0;i < nx; i++){
     for(let j = 0;j < ny; j++){
 context.strokeRect(i * sqside, j * sqside, sqside, sqside);
-    }
+       }
+   }
 }
 //context.fillRect(herox* sqside,heroy * sqside,sqside,sqside);
-let heroimg = new Image();
-heroimg.src = "hero.png";
-context.drawImage(heroimg,herox* sqside,heroy * sqside,sqside,sqside);
+
+
+//context.drawImage(heroimg,herox* sqside,heroy * sqside,sqside,sqside);
+drawMap ();
+canvas.onclick = function(e) {
+let x = e.x - canvas.offsetLeft;
+let y = e.y - canvas.offsetTop;
+herox = Math.floor(x/sqside);
+heroy = Math.floor(y/sqside);
+drawMap();
+
+}
